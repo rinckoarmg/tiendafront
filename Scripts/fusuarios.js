70e0ea1a7ed25778d4200ac0458c4663bdf46e2d
rinckarmg <rinckoar@gmail.com>
2021-09-30 20:17:15 -0500

    cambios user

--- Scripts/fusuarios.js
+++ Scripts/fusuarios.js
@@ -1,12 +1,3 @@
 const D = document,
-$form = D.getElementById("datos_usuario").content;
+$formulario = D.getElementById("datos_usuario");
 
-$form.getElementById("InputCedula").textContent = usuario.cedula_usuario;
-$form.getElementById("InputEmail").textContent = usuario.email_usuario;
-$form.getElementById("InputNombre").textContent = usuario.nombre_usuario;
-$form.getElementById("InputPassword").textContent = usuario.password;
-$form.getElementById("InputUsuario").textContent = usuario.usuario;
-
-D.addEventListener("click", async e =>{
-
-});
