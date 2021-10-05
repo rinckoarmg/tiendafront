0c8377d5501b3f4441c2447000f5cd834e761e8f
rinckarmg <rinckoar@gmail.com>
2021-10-04 21:45:58 -0500

    buscar + buscar by id listo

--- Scripts/productos.js
+++ Scripts/productos.js
@@ -9,58 +9,69 @@ $codigo = D.getElementById("codigoproducto").nodeValue;
 //metodo GET 
 
 const listaPr = async()=>{
-    if ($codigo==undefined){
+    try {
+        let res = await fetch("http://localhost:8080/productos/listar"),
+        json = await res.json();
+        if (!res.ok) throw{status:res.status,statusText:res.statusText}; 
+        console.log(json);
+        json.forEach(producto => {
+            $template.getElementById("codigo_producto").textContent = producto.codigo_producto;
+            $template.getElementById("iva_compra").textContent = producto.ivacompra;
+            $template.getElementById("nit_proveedor").textContent = producto.nitproveedor.nitproveedor;
+            $template.getElementById("nombre_producto").textContent = producto.nombre_producto;
+            $template.getElementById("precio_compra").textContent = producto.precio_compra;
+            $template.getElementById("precio_venta").textContent = producto.precio_venta;
+            $template.getElementById("eliminar_producto").dataset.codigo_producto = producto.codigo_producto;
+            let $clone = D.importNode($template, true);
+            $fragmento.appendChild($clone);
+        }); 
+        $tabla.querySelector("tbody").appendChild($fragmento);
+    } catch (error) {
+        let mensaje=err.statusText("Ocurrio un error");
+    }
+      
+}
+D.addEventListener("DOMContentLoaded",listaPr);
+
+// Metodo GET by Id
+
+D.addEventListener("submit", async (e) =>{
+    if (e.target==$buscar){
+        $tabla.querySelector("tbody").textContent="";
+        e.preventDefault();
         try {
-            let res = await fetch("http://localhost:8080/productos/listar"),
-            json = await res.json();
+            let res = await fetch(`http://localhost:8080/productos/buscar/${e.target.codigoproducto.value}`),
+            json = await res.json(); 
             if (!res.ok) throw{status:res.status,statusText:res.statusText}; 
             console.log(json);
-            json.forEach(producto => {
-                $template.getElementById("codigo_producto").textContent = producto.codigo_producto;
-                $template.getElementById("iva_compra").textContent = producto.ivacompra;
-                $template.getElementById("nit_proveedor").textContent = producto.nitproveedor.nitproveedor;
-                $template.getElementById("nombre_producto").textContent = producto.nombre_producto;
-                $template.getElementById("precio_compra").textContent = producto.precio_compra;
-                $template.getElementById("precio_venta").textContent = producto.precio_venta;
-                $template.getElementById("eliminar_producto").dataset.codigo_producto = producto.codigo_producto;
+                $template.getElementById("codigo_producto").textContent = json.codigo_producto;
+                $template.getElementById("iva_compra").textContent = json.ivacompra;
+                $template.getElementById("nit_proveedor").textContent = json.nitproveedor.nitproveedor;
+                $template.getElementById("nombre_producto").textContent = json.nombre_producto;
+                $template.getElementById("precio_compra").textContent = json.precio_compra;
+                $template.getElementById("precio_venta").textContent = json.precio_venta;
                 let $clone = D.importNode($template, true);
                 $fragmento.appendChild($clone);
-            }); 
             $tabla.querySelector("tbody").appendChild($fragmento);
         } catch (error) {
             let mensaje=err.statusText("Ocurrio un error");
         }
-    } else {
-        D.addEventListener("submit", async (e) =>{
-            if (e.target===$buscar){
-                e.preventDefault();
-                try {
-                    let res = await fetch(`http://localhost:8080/productos/buscar/${e.target.codigoproducto.value}`),
-                    json = await res.json(); 
-                    if (!res.ok) throw{status:res.status,statusText:res.statusText}; 
-                    console.log(json);
-                        $template.getElementById("codigo_producto").textContent = json.codigo_producto;
-                        $template.getElementById("iva_compra").textContent = json.ivacompra;
-                        $template.getElementById("nit_proveedor").textContent = json.nitproveedor.nitproveedor;
-                        $template.getElementById("nombre_producto").textContent = json.nombre_producto;
-                        $template.getElementById("precio_compra").textContent = json.precio_compra;
-                        $template.getElementById("precio_venta").textContent = json.precio_venta;
-                        let $clone = D.importNode($template, true);
-                        $fragmento.appendChild($clone);
-                    $tabla.querySelector("tbody").appendChild($fragmento);
-                } catch (error) {
-                    let mensaje=err.statusText("Ocurrio un error");
-                }
-            }
-        })
-    }     
+    }
+    limpiar();
+})
+
+// Limpiar codigo producto
+function limpiar() {
+    D.getElementById("codigoproducto").value = "";
 }
-D.addEventListener("DOMContentLoaded",listaPr);
 
-// Metodo GET
-
-
-
+// Cargar listado
+D.addEventListener("click", async e =>{
+    if (e.target.matches("#vertodos")){
+        $tabla.querySelector("tbody").textContent="";
+        listaPr();
+    }
+})
 
 // Metodo DELETE
 D.addEventListener("click", async e =>{
