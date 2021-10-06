02f760cee793d55928c17f5b96fb0f941dd89b86
rinckarmg <rinckoar@gmail.com>
2021-10-06 18:30:02 -0500

    modal completos

--- Scripts/reportes.js
+++ Scripts/reportes.js
@@ -1,7 +1,11 @@
 const D = document,
 $tabla = D.getElementById("tabla_ventas"), 
 $template = D.getElementById("listado_ventas").content,
-$fragmento = D.createDocumentFragment();
+$buscar = D.getElementById("buscarVenta"),
+$fragmento = D.createDocumentFragment(),
+$codigo = D.getElementById("codigoventa").nodeValue,
+$subtotal = 0,
+$cajatotal = D.getElementById("totalVentas").content;
 
 //metodo GET 
 const listaV = async()=>{
@@ -14,13 +18,56 @@ const listaV = async()=>{
             $template.getElementById("cedulaV").textContent = venta.cedula_cliente.cedula_cliente;
             $template.getElementById("nombreV").textContent = venta.cedula_cliente.nombre_cliente;
             $template.getElementById("valorTotalV").textContent = venta.total_venta;
-            $template.getElementById("eliminar_venta").dataset.codigo_venta = venta.codigo_venta;
+            //$template.getElementById("eliminar_venta").dataset.codigo_venta = venta.codigo_venta;
             let $clone = D.importNode($template, true);
             $fragmento.appendChild($clone);
+            //$subtotal = venta.total_venta + $subtotal;
         }); 
         $tabla.querySelector("tbody").appendChild($fragmento);
+        //$cajatotal.appendChild($subtotal);
     } catch (error) {
         let mensaje=err.statusText("Ocurrio un error");
     }
 }
 D.addEventListener("DOMContentLoaded",listaV);
+
+// Metodo GET by Id
+D.addEventListener("submit", async (e) =>{
+    if (e.target==$buscar){
+        $tabla.querySelector("tbody").textContent="";
+        e.preventDefault();
+        try {
+            let res = await fetch(`http://localhost:8080/ventas/buscar/${e.target.cedula_cliente.value}`),
+            json = await res.json(); 
+            if (!res.ok) throw{status:res.status,statusText:res.statusText}; 
+            console.log(json);
+            
+                $template.getElementById("cedulaV").textContent = venta.cedula_cliente.cedula_cliente;
+                $template.getElementById("nombreV").textContent = venta.cedula_cliente.nombre_cliente;
+                $template.getElementById("valorTotalV").textContent = venta.total_venta;
+                    let $clone = D.importNode($template, true);
+                    $fragmento.appendChild($clone);
+            
+            $tabla.querySelector("tbody").appendChild($fragmento);
+        } catch (error) {
+            let mensaje=err.statusText("Ocurrio un error");
+        }
+    }
+    D.getElementById("codigoventa").value = "";
+})
+
+// Cargar listado
+D.addEventListener("click", async e =>{
+    if (e.target.matches("#vertodos")){
+        $tabla.querySelector("tbody").textContent="";
+        listaV();
+    }
+})
+
+// totales 
+/*async function totalizar(){
+
+    total = $template.getElementById("valorTotalV").dataset.total_venta + total;
+    $cajatotal.appendChild(acumulado)  
+}
+D.addEventListener("DOMContentLoaded",totalizar);*/
